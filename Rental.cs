64794db00628b225c045989d20b94dcdf06a5574
meteornium <79771326+meteornium@users.noreply.github.com>
2021-05-21 13:12:28 -0500

    Create Rental.cs

---
+++ Rental.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class Rental
+    {
+        [Key]
+        public int RentalId { get; set; }
+        public string RentalName { get; set; }
+        public int RentalCost { get; set; }
+        public string FlawsAndDamages { get; set; }
+    }
+}
